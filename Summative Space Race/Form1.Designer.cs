72e26247f9a30a31bc3eb5c2e346851295047427
Heet Patel <Heet Patel@WINDOWS-43PV03O>
2024-05-20 20:50:14 -0400

    not done

--- Summative Space Race/Form1.Designer.cs
+++ Summative Space Race/Form1.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.components = new System.ComponentModel.Container();
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.player2ScoreLabel = new System.Windows.Forms.Label();
+            this.player1ScoreLabel = new System.Windows.Forms.Label();
+            this.winLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // gameTimer
@@ -38,11 +41,47 @@
             this.gameTimer.Interval = 20;
             this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
             // 
+            // player2ScoreLabel
+            // 
+            this.player2ScoreLabel.AutoSize = true;
+            this.player2ScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.player2ScoreLabel.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.player2ScoreLabel.Location = new System.Drawing.Point(684, 9);
+            this.player2ScoreLabel.Name = "player2ScoreLabel";
+            this.player2ScoreLabel.Size = new System.Drawing.Size(20, 24);
+            this.player2ScoreLabel.TabIndex = 0;
+            this.player2ScoreLabel.Text = "0";
+            // 
+            // player1ScoreLabel
+            // 
+            this.player1ScoreLabel.AutoSize = true;
+            this.player1ScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.player1ScoreLabel.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.player1ScoreLabel.Location = new System.Drawing.Point(103, 9);
+            this.player1ScoreLabel.Name = "player1ScoreLabel";
+            this.player1ScoreLabel.Size = new System.Drawing.Size(20, 24);
+            this.player1ScoreLabel.TabIndex = 1;
+            this.player1ScoreLabel.Text = "0";
+            // 
+            // winLabel
+            // 
+            this.winLabel.BackColor = System.Drawing.Color.Transparent;
+            this.winLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 21F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winLabel.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.winLabel.Location = new System.Drawing.Point(171, 180);
+            this.winLabel.Name = "winLabel";
+            this.winLabel.Size = new System.Drawing.Size(441, 43);
+            this.winLabel.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.player1ScoreLabel);
+            this.Controls.Add(this.player2ScoreLabel);
+            this.Controls.Add(this.winLabel);
             this.DoubleBuffered = true;
             this.Name = "Form1";
             this.Text = "Form1";
@@ -51,12 +90,16 @@
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label player2ScoreLabel;
+        private System.Windows.Forms.Label player1ScoreLabel;
+        private System.Windows.Forms.Label winLabel;
     }
 }
 
--- Summative Space Race/Form1.cs
+++ Summative Space Race/Form1.cs
@@ -12,22 +12,26 @@ namespace Summative_Space_Race
 {
     public partial class Form1 : Form
     {
-        //Hero variables
-        Rectangle hero = new Rectangle(280, 100, 10, 10);
-        int heroSpeed = 10;
+        //Players variables
+        Rectangle player1 = new Rectangle(380, 200, 10, 10);
+        Rectangle player2 = new Rectangle(80, 200, 10, 10);
+        int playersSpeed = 5;
 
         //Ball variables
         int ballSize = 10;
         int ballSpeed = 8;
 
         //List of balls
-        List<Rectangle> ballList = new List<Rectangle>();
+        List<Rectangle> ballLeft = new List<Rectangle>();
+        List<Rectangle> ballright = new List<Rectangle>();
 
         int score = 0;
         int time = 500;
 
         bool upPressed = false;
         bool downPressed = false;
+        bool wPressed = false;
+        bool sPressed = false;
 
         SolidBrush greenBrush = new SolidBrush(Color.Green);
         SolidBrush whiteBrush = new SolidBrush(Color.White);
@@ -47,7 +51,18 @@ namespace Summative_Space_Race
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            e.Graphics.FillRectangle(greenBrush, hero);
+            //Drawing Players
+            e.Graphics.FillRectangle(greenBrush, player1);
+            e.Graphics.FillRectangle(greenBrush, player2);
+
+            for (int i = 0; i < ballLeft.Count; i ++)
+            {
+                e.Graphics.FillEllipse(whiteBrush, ballLeft[i]);
+            }
+            for (int i = 0; i < ballright.Count; i++)
+            {
+                e.Graphics.FillEllipse(whiteBrush, ballright[i]);
+            }
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
@@ -59,6 +74,12 @@ namespace Summative_Space_Race
                     break;
                 case Keys.Down:
                     downPressed = true;
+                    break;
+                case Keys.W:
+                    wPressed = true;
+                    break;
+                case Keys.S:
+                    sPressed = true;
                     break;
             }
         }
@@ -73,27 +94,80 @@ namespace Summative_Space_Race
                 case Keys.Down:
                     downPressed = false;
                     break;
+                case Keys.S: 
+                    sPressed = false;
+                    break;
+                case Keys.W:
+                    wPressed = false;
+                    break;
             }
         }
 
         private void gameTimer_Tick(object sender, EventArgs e)
         {
-            //move player 
-            if (leftPressed == true && hero.X > 0)
+            //All commands for Player 1
+
+            //move player 1
+            if (upPressed == true && player1.Y > 0)
             {
-                hero.X = hero.X - heroSpeed;
+                player1.Y = player1.Y - playersSpeed;
             }
-            if (rightPressed == true && hero.X < this.Width - hero.Width)
+            if (downPressed == true && player1.Y < this.Height - player1.Height)
             {
-                hero.X = hero.X + heroSpeed;
+                player1.Y = player1.Y + playersSpeed;
             }
-            if (upPressed == true && hero.Y > 0)
+
+            //All commands for Player 2
+
+            //move player 2
+            if (wPressed == true && player2.Y > 0)
             {
-                hero.Y = hero.Y - heroSpeed;
+                player2.Y = player2.Y - playersSpeed;
             }
-            if (downPressed == true && hero.Y < this.Height - hero.Height)
+            if (sPressed == true && player2.Y < this.Height - player2.Height)
             {
-                hero.Y = hero.Y + heroSpeed;
+                player2.Y = player2.Y + playersSpeed;
+            }
+
+            //All commands for ballleft 
+            //create new ball if it is time and different colour //////reword
+            randValue = randGen.Next(0, 100);
+
+            if (randValue < 30)
+            {
+                if (randValue < 25)
+                {
+                    randValue = randGen.Next(0, this.Height);
+
+                    Rectangle ball = new Rectangle(0, randValue, ballSize, ballSize);
+                    ballLeft.Add(ball);
+                }
+            }
+
+            //Make obstacles move left to right.
+            for (int i = 0; i < ballLeft.Count; i++)
+            {
+                int x = ballLeft[i].X + ballSpeed;
+                ballLeft[i] = new Rectangle(x, ballLeft[i].Y, ballSize, ballSize);
+            }
+
+            //All commands for ballright
+            if (randValue < 30)
+            {
+                if (randValue < 25)
+                {
+                    randValue = randGen.Next(0, this.Height);
+
+                    Rectangle ball = new Rectangle(0, randValue, ballSize, ballSize);
+                    ballright.Add(ball);
+                }
+            }
+
+            //Make obstacles move left to right.
+            for (int i = 0; i < ballright.Count; i++ )
+            {
+                int x = ballright[i].X - ballSpeed;
+                ballright[i] = new Rectangle(x, ballright[i].Y, ballSize, ballSize);
             }
 
             Refresh();
